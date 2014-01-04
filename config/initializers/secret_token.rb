b558218d44d4001d84c4b9647c2f16a372c14a0f
HIGAKI Masaru <mas.higa@gmail.com>
2014-01-04 17:20:19 +0900

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CANELEduJAPON::Application.config.secret_key_base = '97f6c466d713033acc7ed63ed2fcc1cdb96e4bd7e250a612d2d0c182ff4498764c82854622984b0a6b69d3d94a552f2ea13b330842c6ae47bf9b6f971a527d57'
